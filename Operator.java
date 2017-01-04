0616939ab2a11e4eb244d2b53ea7d4b0c3b71ac5
ASHISH1503 <ashishranjan988@gmail.com>
2017-01-04 16:09:05 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,20 @@
+
+public class Operator {
+
+	public static void main(String[] args)
+	{
+	     int a;
+	     a = 20;
+	     int b = 10;
+	      b = a-- - --a;
+	     int c = a--;
+	     int d = a>>2;
+	     int e = a&b;
+	     
+			System.out.println("the new value of b : " +b);
+	        System.out.println("the value of c : " +c);
+	        System.out.println("the value of d : " +d);
+	        System.out.println("the value of e : " +e);
+	       
+	    }
+}
